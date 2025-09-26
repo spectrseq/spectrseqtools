6de3b01f6245e1f3ead4f423ba72dc0bdc308b4e
Juan Magalang <21294515+jarmsmagalang@users.noreply.github.com>
2025-09-26 18:53:27 +0200

    Scan clustering considered in singleton identification

--- lionelmssq/singleton_matching.py
+++ lionelmssq/singleton_matching.py
@@ -2,6 +2,9 @@ import polars as pl
 import tqdm as tqdm
 from lionelmssq.masses import ELEMENT_MASSES, PHOSPHATE_LINK_MASS, MASSES
 from clr_loader import get_mono
+from dbscan1d.core import DBSCAN1D
+from sklearn.metrics import silhouette_score
+
 rt = get_mono()
 
 PPM_TOLERANCE = 10
@@ -23,6 +26,7 @@ THEO_MZ_MIN = UNIQUE_MASSES["theoretical_mz"].min() - (UNIQUE_MASSES["theoretica
 
 def generate_mz_df(bunch):
     scan_id = int(bunch.scan_id.split("scan=")[-1])
+    scan_time = bunch.scan_time
 
     b_peak_index = []
     b_intensity = []
@@ -37,12 +41,48 @@ def generate_mz_df(bunch):
                 b_intensity.append(bunch.peaks[i].intensity)
                 b_mz.append(peak_mz)
         mz_df = pl.DataFrame(data = {"scan_id": scan_id,
-                                        "peak_index": b_peak_index,
-                                        "intensity": b_intensity,
-                                        "mz": b_mz})
+                                     "scan_time": scan_time,
+                                     "peak_index": b_peak_index,
+                                     "intensity": b_intensity,
+                                     "mz": b_mz})
         return mz_df
     else:
         return None
+
+def cluster_score(scantimes):
+    """
+    Provide a score on how clustered each of the scan peaks by scan time, using DBSCAN and Silhouette scores
+
+    Parameters
+    ----------
+    scantimes : polars Series
+        Series containing scan times
+
+    Returns
+    -------
+    score : float
+        Silhouette score for the DBSCAN cluster of scan times
+    """
+
+    scan_arr = scantimes.sort().to_numpy()
+
+    #Cluster scan times using 1D DBSCAN
+    dbs = DBSCAN1D(eps = 0.5, min_samples = 10)
+    clusters = dbs.fit_predict(scan_arr)
+
+    X = scan_arr.reshape(-1, 1)
+    if len(set(clusters)) == 1:
+        #If there are only noise clusters (cluster = -1), set score to -1.0
+        if list(set(clusters))[0] == -1:
+            score = -1.0
+        #If there is only one cluster, set score to 0.0
+        else:
+            score = 0.0
+    else:
+        #Compute silhouette score otherwise
+        score = silhouette_score(X, clusters)
+
+    return score
 
 def match_singletons(df_mz):
     """
@@ -59,30 +99,45 @@ def match_singletons(df_mz):
         Dataframe containing list of candidate nucleosides obtained by matching singletons
     """
 
-    #Group observed m/z together on the cluster index, aggregate by taking the mean of the m/z
-    df_mz_agg = df_mz.group_by("ppm_group").agg(observed_mz = pl.col("mz").mean(),
-                                                count = pl.len()).sort("observed_mz")
-    
-    #Match mean observed m/z to theoretical m/z from the reference table 
-    df_mz_agg = (df_mz_agg.sort("observed_mz")
-                          .join_asof(UNIQUE_MASSES.sort("theoretical_mz"),
-                                     left_on = "observed_mz",
-                                     right_on = "theoretical_mz",
-                                     strategy = "nearest"))
+    #Match m/z to theoretical m/z from the reference table 
+    df_mz = (
+        df_mz.sort("mz")
+        .join_asof(UNIQUE_MASSES.sort("theoretical_mz"),
+                   left_on = "mz",
+                   right_on = "theoretical_mz",
+                   strategy = "nearest"
+                   )
+            )
     
     #Compute mass error between observed and theoretical m/z
-    df_mz_agg = df_mz_agg.sort("observed_mz").with_columns((1e6*abs(pl.col("observed_mz")
-                                                                    -pl.col("theoretical_mz"))
-                                                                    /pl.col("observed_mz"))
-                                            .fill_null(0).fill_nan(0)
-                                            .lt(PPM_TOLERANCE).alias("is_match"))
+    df_mz = (
+        df_mz.sort("mz")
+        .with_columns((1e6*abs(pl.col("mz")
+                               -pl.col("theoretical_mz"))
+                               /pl.col("mz")
+                        ).fill_null(0).fill_nan(0)
+                        .lt(PPM_TOLERANCE).alias("is_match")
+                    )
+        .filter(pl.col("is_match"))
+        .sort(["nucleoside", "scan_time"])
+            )
     
+    df_mz_agg = (
+        df_mz.group_by("nucleoside")
+        .map_groups(lambda x: pl.DataFrame({"nucleoside": x["nucleoside"][0],
+                                            "cluster_score": cluster_score(x["scan_time"]),
+                                            "count": len(x["nucleoside"])
+                                            })
+                    )
+                )
+
     #Take mass error that is less than PPM_TOLERANCE and flatten list of candidate singletons
     df_matches = (
-        df_mz_agg.filter(pl.col("is_match"))
-                 .select(["nucleoside", "count"])
+        df_mz_agg.filter(pl.col("cluster_score") >= 0)
+                 .select(["nucleoside", "count", "cluster_score"])
                  .sort("count", descending = True)
-                 .explode("nucleoside"))
+                 #.explode("nucleoside")
+                 )
     
     return df_matches
 
@@ -107,7 +162,7 @@ def match_singletons_only(raw_file_read):
     bunch = next(raw_file_read)
 
     mz_list = []
-    for _ in tqdm.tqdm(range(len(raw_file_read)-1), desc = "Deisotoping MS2 scans"):
+    for _ in tqdm.tqdm(range(len(raw_file_read)-1), desc = "Matching singletons only (no deisotoping)"):
         if bunch.ms_level == 2: #Only consider MS2 scans
 
             #Centroid the scans
